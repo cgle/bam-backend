fc390a14f54ec45129f3a2e08442bcea65af5131
cgle <cle@macalester.edu>
2014-11-04 01:51:28 -0600

    S3 aws integration

--- app/utils.js
+++ app/utils.js
@@ -1,6 +1,11 @@
 var redis = require('redis');
 var config = require('../config/app');
 var redisClient = redis.createClient(config.redis_port, config.redis_host);
+var knox = require('knox').createClient({
+  key: config.s3_key,
+  secret: config.s3_secret,
+  bucket: config.s3_bucket
+});
 
 redisClient.auth(config.redis_pwd);
 redisClient.on('error', function(err) {
@@ -13,3 +18,4 @@ redisClient.on('connect', function() {
 
 exports.redis = redis;
 exports.redisClient = redisClient;
+exports.knox = knox;
--- config/app.js
+++ config/app.js
@@ -3,5 +3,8 @@ module.exports = {
   url: 'mongodb://bam:dollhouse@ds049170.mongolab.com:49170/bam',
   redis_port: '14253',
   redis_host: 'pub-redis-14253.us-east-1-2.2.ec2.garantiadata.com',
-  redis_pwd: 'dollhouse'
+  redis_pwd: 'dollhouse',
+  s3_key: 'AKIAJA26MCHHDFNZ2D2Q',
+  s3_secret: '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',
+  s3_bucket: 'bam-static'
 };
