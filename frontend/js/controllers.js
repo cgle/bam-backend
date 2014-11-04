eb3268dad9827bff64acc4dc92f76d398bf605c7
Paul Yeo 2 <paulyeo2@Pauls-MacBook-Pro.local>
2014-11-03 22:38:36 -0600

    add sort button

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -3,13 +3,21 @@
 /* Controllers */
 var bamApp = angular.module('bamApp', []);
 
-bamApp.controller('EventsListCtrl', function($scope) {
-	$scope.events = [
-		{'name':'Turck 2 Progressive', 
-			'snippet':'Party woohoo!'},
-		{'name': 'Dollhouse Rager Party',
-    		'snippet': 'Cuong is doing a keg stand.'},
-    	{'name': 'Leonard Center Basketball',
-     		'snippet': 'We hooping at the LC at 7 pm come through.'}
+bamApp.controller('EventsListCtrl', ['$scope', 
+  function($scope) {
+  	$scope.events = [
+  		{'name':'Turck 2 Progressive', 
+  		 'snippet':'Party woohoo!',
+       'date':'2003Mar20',
+  		 'age':2},
+  		{'name': 'Dollhouse Rager Party',
+       'snippet': 'Cuong is doing a keg stand.',
+       'date':'1999Jan3',
+       'age':3},
+      {'name': 'Leonard Center Basketball',
+       'snippet': 'We hooping at the LC at 7 pm come through.',
+       'date':'2014Nov3',
+       'age':1}
     ];
-});
+    $scope.orderProp = 'age';
+}]);
--- frontend/test/e2e/scenarios.js
+++ frontend/test/e2e/scenarios.js
@@ -9,28 +9,54 @@ describe('Bam!', function() {
 		beforeEach(function() {
 		browser.get('index.html');
 		});
-		
-		var eventsList = element.all(by.repeater('event in events'));
-		var query = element(by.model('query'));
 
 		it('should filter the events list as a user types into the search box', function() {
+			var eventsList = element.all(by.repeater('event in events'));
+			var query = element(by.model('query'));
 
-		expect(eventsList.count()).toBe(3);
+			expect(eventsList.count()).toBe(3);
 
-		query.sendKeys('Turck');
-		expect(eventsList.count()).toBe(1);
+			query.sendKeys('Turck');
+			expect(eventsList.count()).toBe(1);
 
-		query.clear();
-		query.sendKeys('Party');
-		expect(eventsList.count()).toBe(2);
+			query.clear();
+			query.sendKeys('Party');
+			expect(eventsList.count()).toBe(2);
 		});
 
 		it('should display the current filter value in the title bar', function() {
+			var query = element(by.model('query'));
+
 		  query.clear();
 		  expect(browser.getTitle()).toMatch(/Bam!:\s*$/);
 
 		  query.sendKeys('Party');
 		  expect(browser.getTitle()).toMatch(/Bam!: Party$/);
+		});
+
+		it('should be possible to control event order via the drop down select box', function(){
+			var eventNameColumn = element.all(by.repeater('event in events').column('event.name'));
+			var query = element(by.model('query'));
+			
+			function getNames() {
+		    return eventNameColumn.map(function(elm) {
+		      return elm.getText();
+		    });
+		  }
+		  
+		  query.sendKeys('Party'); //let's narrow the dataset to make the test assertions shorter
+
+		  expect(getNames()).toEqual([
+		    "Turck 2 Progressive",
+		    "Dollhouse Rager Party"
+		  ]);
+
+		  element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+
+		  expect(getNames()).toEqual([
+		    "Dollhouse Rager Party",
+		    "Turck 2 Progressive"
+		  ]);
 		});
 	});
 });
