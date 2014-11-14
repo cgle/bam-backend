8fa75de4902c3f3580a8684ae68d2c6c1b60904e
Paul Yeo <paulyeo21@gmail.com>
2014-11-14 03:35:14 -0600

    button changes

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,11 +1,24 @@
 'use strict';
 
 $(document).ready(function() {
-    $(".login-button").on("click", function() {
+
+    $(".login-button").click(function() {
         $(".overlay").addClass('overlay-open');
     });
 
     $(".overlay-close").click(function() {
         $(".overlay").removeClass("overlay-open");
     });
+
+    $(".search-button").click(function() {
+        $(".search-flyout").addClass('search-open');
+    });
+
+    $(".sections-button").click(function() {
+        $(".dropdown").addClass('dropdown-open');
+    });
+
+    // if(!$(event.target).is('.search-container')) {
+    //     $(".search-flyout").removeClass("search-open");
+    // }
 });
