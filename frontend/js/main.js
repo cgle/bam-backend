30abcea6dff879a23c9d3b2d7dc8bc37c2602f38
Rhyan <rfookune@macalester.edu>
2014-11-29 21:33:26 -0600

    close overlay implimented

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -55,3 +55,13 @@ function getUserData(userId) {
         url: '/api/users/' + userId,
     });
 }
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block'){
+        e.style.display = 'none';
+    }
+    else{
+        e.style.display = 'block';
+    }
+}
