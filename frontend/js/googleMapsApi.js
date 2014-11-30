530a27c3d7e8d59abc7992686e54ac0cf11bf6d3
Kohei Hisakuni <khisakun@macalester.edu>
2014-11-29 21:44:13 -0600

    Fixed event form address input

--- frontend/js/googleMapsApi.js
+++ frontend/js/googleMapsApi.js
@@ -1,21 +1,18 @@
 console.log('google maps might be working?');
 
 var placeSearch, autocomplete;
-var componentForm = {
-  street_number: 'short_name',
-  route: 'long_name',
-  locality: 'long_name',
-  administrative_area_level_1: 'short_name',
-  country: 'long_name',
-  postal_code: 'short_name'
-};
+
 
 function initialize() {
   // Create the autocomplete object, restricting the search
   // to geographical location types.
   autocomplete = new google.maps.places.Autocomplete((document.getElementById('googleAddress')),
       { types: ['geocode'] });
-  console.log('initialize working');
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    console.log(">>>>", $('#googleAddress').val());
+    $('#googleAddress').val(fillInAddress()).change();
+  });
 }
 
 function loadScript() {
@@ -25,6 +22,12 @@ function loadScript() {
       'callback=initialize';
   document.body.appendChild(script);
   console.log('load script called');
+}
+
+function fillInAddress(){
+  var place = autocomplete.getPlace();
+  console.log(place.formatted_address);
+  return place.formatted_address;
 }
 
 window.onload = loadScript();
