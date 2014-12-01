ea932f005fbb7a5194a1bd84c1b68043f74af304
paulyeo21 <paulyeo21@gmail.com>
2014-11-30 19:38:25 -0600

    isotope

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -36,4 +36,14 @@ $(document).ready(function() {
     }).mouseout(function() {
         $(this).css("opacity","1.0");
     })
+    
+    function toggle_visibility(id) {
+        var e = document.getElementById(id);
+        if(e.style.display == 'block'){
+            e.style.display = 'none';
+        }
+        else{
+            e.style.display = 'block';
+        }
+    }
 });
